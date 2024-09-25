3b425d7fcee3db1bc8fa666f9c93cdd54943822a
vanguard <vanguard09@naver.com>
2024-09-25 13:33:14 +0900

    image repository 임시구현

---
+++ src/main/java/com/restapi/project_AI_diary_backend/domain/image/repository/ImageRepository.java
@@ -0,0 +1,15 @@
+package com.restapi.project_AI_diary_backend.domain.image.repository;
+
+import com.restapi.project_AI_diary_backend.domain.image.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    // 특정 다이어리의 모든 이미지 반환
+    List<Image> findByDiary_DiaryId(Long diaryId);
+    // 생성 날짜로 이미지 조회
+    List<Image> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+}
