d4321391f3b24b1b4fa3e3b7ad808d0a1d9a6455
vanguard <vanguard09@naver.com>
2024-09-25 13:24:00 +0900

    image entity 추가

---
+++ src/main/java/com/restapi/project_AI_diary_backend/domain/image/entity/Image.java
@@ -0,0 +1,39 @@
+package com.restapi.project_AI_diary_backend.domain.image.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.restapi.project_AI_diary_backend.domain.diary.entity.Diary;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "image")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id", nullable = false)
+    private long imageId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "diary_id", nullable = false)
+    private Diary diary;
+
+    @Column(name = "file_name", length = 100, nullable = false)
+    private String fileName;
+
+    @Column(name = "is_select", nullable = false)
+    private boolean isSelect;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
